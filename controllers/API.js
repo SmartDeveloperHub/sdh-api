5fa44c1471f6f96f4f79217b54c8942949d7550a
Carlos Blanco <cblanco@conwet.com>
2015-04-27 13:58:23 +0200

    Improve user and metric templates

--- controllers/API.js
+++ controllers/API.js
@@ -7,13 +7,14 @@ var API = require('./APIService');
 
 
 module.exports.apiInfo = function apiInfo (req, res, next) {
+  
 
-	var result = API.apiInfo();
+  var result = API.apiInfo();
 
-	if(typeof result !== 'undefined') {
-		res.setHeader('Content-Type', 'application/json');
-		res.end(JSON.stringify(result || {}, null, 2));
-	}
-	else
-		res.end();
+  if(typeof result !== 'undefined') {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(result || {}, null, 2));
+  }
+  else
+    res.end();
 };
