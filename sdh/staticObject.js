bf1cdbd9dd827e98182339bdc5896417fc9a6c2e
Carlos Blanco <cblanco@conwet.com>
2015-06-25 22:24:11 +0200

    Improve static data load

--- sdh/staticObject.js
+++ sdh/staticObject.js
@@ -33,40 +33,72 @@ var _metricsById;
 var _tbdById;
 var _staticInfoById;
 
-module.exports.preloadAll = function preloadAll (callback) {
+var getStaticUsersRepos = function getStaticUsersRepos(returnCallback) {
     // TODO get from SDH platform
-    _repositories = require('./fakeRepositoriesInfo.js');
-    _users = require('./fakeUsersInfo.js');
-    _defaultDateRange = {'from': new Date("Thu Apr 1 2015"), 'to': new Date("Thu Apr 25 2015")};
+    var _reps = require('./fakeRepositoriesInfo.js');
+    // ?s a scm:Repository
+    // n3 parse and get Repositories list like this:
+    /*
+     [
+         {
+             "repositoryid": "string",
+             "name": "string",
+             "description": "string"
+         }
+     ]
+     */
+    var _usrs = require('./fakeUsersInfo.js');
+    // ?s a scm:User
+    // n3 parse and get Users list like this:
+    /*
+     [
+         {
+             "userid": "string",
+             "name": "string",
+             "email": "string",
+             "description": "string"
+         }
+     ]
+    */
+    returnCallback(_usrs, _reps);
+};
 
-    _usersById = {};
-    _repositoriesById = {};
-    _metricsById = {};
-    _tbdById = {};
-    _staticInfoById = {};
+module.exports.preloadAll = function preloadAll (callback) {
+    var nextStep = function (usrs, reps) {
+        _repositories = reps;
+        _users = usrs;
+        _defaultDateRange = {'from': new Date("Thu Apr 1 2015"), 'to': new Date("Thu Apr 25 2015")};
 
-    // Static data structures generation
-    for(var i = 0; i < metrics.metrics.length; i++) {
-        _metricsById[metrics.metrics[i].id] = metrics.metrics[i];
-    }
-    for(var i = 0; i < tbd.tbd.length; i++) {
-        _tbdById[tbd.tbd[i].id] = tbd.tbd[i];
-    }
-    for(var i = 0; i < _users.fakeUsersInfo.length; i++) {
-        _usersById[_users.fakeUsersInfo[i].userid] = _users.fakeUsersInfo[i];
-    }
-    for(var i = 0; i < _repositories.fakeRepositoriesInfo.length; i++) {
-        _repositoriesById[_repositories.fakeRepositoriesInfo[i].repositoryid] = _repositories.fakeRepositoriesInfo[i];
-    }
-    // Make global all this methods for param validation
-    GLOBAL.metricsById = this.getMetricsById();
-    GLOBAL.repositories = this.getRepositories();
-    GLOBAL.users = this.getUsers();
-    GLOBAL.usersById = this.getUsersById();
-    GLOBAL.repositoriesById = this.getRepositoriesById();
-    GLOBAL.tbdById = this.getTbdById();
-    GLOBAL.defaultDateRange = this.getDefaultDateRange();
-    callback();
+        _usersById = {};
+        _repositoriesById = {};
+        _metricsById = {};
+        _tbdById = {};
+        _staticInfoById = {};
+
+        // Static data structures generation
+        for (var i = 0; i < metrics.metrics.length; i++) {
+            _metricsById[metrics.metrics[i].id] = metrics.metrics[i];
+        }
+        for (var i = 0; i < tbd.tbd.length; i++) {
+            _tbdById[tbd.tbd[i].id] = tbd.tbd[i];
+        }
+        for (var i = 0; i < _users.fakeUsersInfo.length; i++) {
+            _usersById[_users.fakeUsersInfo[i].userid] = _users.fakeUsersInfo[i];
+        }
+        for (var i = 0; i < _repositories.fakeRepositoriesInfo.length; i++) {
+            _repositoriesById[_repositories.fakeRepositoriesInfo[i].repositoryid] = _repositories.fakeRepositoriesInfo[i];
+        }
+        // Make global all this methods for param validation
+        GLOBAL.metricsById = this.getMetricsById();
+        GLOBAL.repositories = this.getRepositories();
+        GLOBAL.users = this.getUsers();
+        GLOBAL.usersById = this.getUsersById();
+        GLOBAL.repositoriesById = this.getRepositoriesById();
+        GLOBAL.tbdById = this.getTbdById();
+        GLOBAL.defaultDateRange = this.getDefaultDateRange();
+        callback();
+    }.bind(this);
+    getStaticUsersRepos(nextStep);
 };
 
 module.exports.getRepositories = function getRepositories () {
