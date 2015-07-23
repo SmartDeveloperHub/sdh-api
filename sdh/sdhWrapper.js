5490fa4dcbe4b085d16e4e17a941d4da7db7456f
Carlos Blanco <cblanco@conwet.com>
2015-07-23 20:54:08 +0200

    Fix bug with repository info request

--- sdh/sdhWrapper.js
+++ sdh/sdhWrapper.js
@@ -342,7 +342,7 @@ var parseRepositoryInfo = function parseRepositoryInfo(data) {
             "name": repoAtts["http://usefulinc.com/ns/doap#name"],
             "description": repoAtts["http://usefulinc.com/ns/doap#description"],
             "tags": tagList,
-            "avatar": repositoriesById[repoAtts["http://www.smartdeveloperhub.org/vocabulary/scm#repositoryId"]], //TODO
+            "avatar": repositoriesById[repoAtts["http://www.smartdeveloperhub.org/vocabulary/scm#repositoryId"]].avatar, //TODO
             "archived": repoAtts["http://www.smartdeveloperhub.org/vocabulary/scm#isArchived"],
             "public": repoAtts["http://www.smartdeveloperhub.org/vocabulary/scm#isPublic"],
             "owner": repoAtts["http://www.smartdeveloperhub.org/vocabulary/scm#owner"],
