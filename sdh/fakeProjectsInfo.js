f6278bd2d8a0165ee45c43b3b0b591bdf620f70a
Carlos Blanco <cblanco@conwet.com>
2015-09-16 21:08:39 +0200

    Add new dummy projects file

---
+++ sdh/fakeProjectsInfo.js
@@ -0,0 +1,233 @@
+/*
+
+    #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+      This file is part of the Smart Developer Hub Project:
+        http://www.smartdeveloperhub.org/
+      Center for Open Middleware
+            http://www.centeropenmiddleware.com/
+    #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+      Copyright (C) 2015 Center for Open Middleware.
+    #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+      Licensed under the Apache License, Version 2.0 (the "License");
+      you may not use this file except in compliance with the License.
+      You may obtain a copy of the License at
+                http://www.apache.org/licenses/LICENSE-2.0
+      Unless required by applicable law or agreed to in writing, software
+      distributed under the License is distributed on an "AS IS" BASIS,
+      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+      See the License for the specific language governing permissions and
+     limitations under the License.
+    #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+*/
+
+'use strict';
+
+exports.fakeProjectsInfo = [
+    {
+        "name" : "SDH-Test1",
+        "description" : "A project",
+        "repositoryid" : "1",
+        "lastcommit" : 1431208800000,
+        "firstcommit": 1273010400000,
+        "scmlink" : "http://gitlabProjectURL",
+        "creation" : 1273010400000,
+        "tags" :['SDH','jordi','dark'],
+        "avatar": "http://ProjectAvatarURL",
+        "repositories": [
+            {
+                "name" : "ALPHA jin",
+                "description" : "A repository",
+                "repositoryid" : "1",
+                "tags" :['framework','hack','dark'],
+                "avatar": "http://repositoryAvatarURL",
+                "archived" : true,
+                "public" : true,
+                "owner" : 'u1',
+                "users": [
+                    {
+                        "name" : "Jos\u00e9 Mart\u00ednez",
+                        "email" : "pepe@organization.com",
+                        "userid" : "1"
+                    },
+                    {
+                        "name" : "Dolores Garc\u00eda",
+                        "email" : "lola@organization.com",
+                        "userid" : "2"
+                    }
+                ]
+            },
+            {
+                "name" : "BETA jang",
+                "description" : "A repository",
+                "repositoryid" : "2",
+                "tags" :['framework','php','laravel'],
+                "avatar": "http://repositoryAvatarURL",
+                "archived" : true,
+                "public" : true,
+                "owner" : '2',
+                "users": [
+                    {
+                        "name" : "Jos\u00e9 Mart\u00ednez",
+                        "email" : "pepe@organization.com",
+                        "userid" : "1"
+                    },
+                    {
+                        "name" : "Dolores Garc\u00eda",
+                        "email" : "lola@organization.com",
+                        "userid" : "2"
+                    }
+                ]
+            }
+        ],
+        "users": [
+            {
+                "name" : "Jos\u00e9 Mart\u00ednez",
+                "email" : "pepe@organization.com",
+                "userid" : "1"
+            },
+            {
+                "name" : "Dolores Garc\u00eda",
+                "email" : "lola@organization.com",
+                "userid" : "2"
+            }
+        ]
+    },
+    {
+        "name" : "SDH-Test2",
+        "description" : "A project",
+        "repositoryid" : "2",
+        "lastcommit" : 1431208800000,
+        "firstcommit": 1273010400000,
+        "scmlink" : "http://gitlabProjectURL",
+        "creation" : 1273010400000,
+        "tags" :['SDH','jordi','dark'],
+        "avatar": "http://ProjectAvatarURL",
+        "repositories": [
+            {
+                "name" : "ALPHA jin",
+                "description" : "A repository",
+                "repositoryid" : "1",
+                "tags" :['framework','hack','dark'],
+                "avatar": "http://repositoryAvatarURL",
+                "archived" : true,
+                "public" : true,
+                "owner" : 'u1',
+                "users": [
+                    {
+                        "name" : "Jos\u00e9 Mart\u00ednez",
+                        "email" : "pepe@organization.com",
+                        "userid" : "1"
+                    },
+                    {
+                        "name" : "Dolores Garc\u00eda",
+                        "email" : "lola@organization.com",
+                        "userid" : "2"
+                    }
+                ]
+            },
+            {
+                "name" : "BETA jang",
+                "description" : "A repository",
+                "repositoryid" : "2",
+                "tags" :['framework','php','laravel'],
+                "avatar": "http://repositoryAvatarURL",
+                "archived" : true,
+                "public" : true,
+                "owner" : '2',
+                "users": [
+                    {
+                        "name" : "Jos\u00e9 Mart\u00ednez",
+                        "email" : "pepe@organization.com",
+                        "userid" : "1"
+                    },
+                    {
+                        "name" : "Dolores Garc\u00eda",
+                        "email" : "lola@organization.com",
+                        "userid" : "2"
+                    }
+                ]
+            }
+        ],
+        "users": [
+            {
+                "name" : "Jos\u00e9 Mart\u00ednez",
+                "email" : "pepe@organization.com",
+                "userid" : "1"
+            },
+            {
+                "name" : "Dolores Garc\u00eda",
+                "email" : "lola@organization.com",
+                "userid" : "2"
+            }
+        ]
+    },
+    {
+        "name" : "SDH-Test3",
+        "description" : "A project",
+        "repositoryid" : "3",
+        "lastcommit" : 1431208800000,
+        "firstcommit": 1273010400000,
+        "scmlink" : "http://gitlabProjectURL",
+        "creation" : 1273010400000,
+        "tags" :['SDH','jordi','dark'],
+        "avatar": "http://ProjectAvatarURL",
+        "repositories": [
+            {
+                "name" : "ALPHA jin",
+                "description" : "A repository",
+                "repositoryid" : "1",
+                "tags" :['framework','hack','dark'],
+                "avatar": "http://repositoryAvatarURL",
+                "archived" : true,
+                "public" : true,
+                "owner" : 'u1',
+                "users": [
+                    {
+                        "name" : "Jos\u00e9 Mart\u00ednez",
+                        "email" : "pepe@organization.com",
+                        "userid" : "1"
+                    },
+                    {
+                        "name" : "Dolores Garc\u00eda",
+                        "email" : "lola@organization.com",
+                        "userid" : "2"
+                    }
+                ]
+            },
+            {
+                "name" : "BETA jang",
+                "description" : "A repository",
+                "repositoryid" : "2",
+                "tags" :['framework','php','laravel'],
+                "avatar": "http://repositoryAvatarURL",
+                "archived" : true,
+                "public" : true,
+                "owner" : '2',
+                "users": [
+                    {
+                        "name" : "Jos\u00e9 Mart\u00ednez",
+                        "email" : "pepe@organization.com",
+                        "userid" : "1"
+                    },
+                    {
+                        "name" : "Dolores Garc\u00eda",
+                        "email" : "lola@organization.com",
+                        "userid" : "2"
+                    }
+                ]
+            }
+        ],
+        "users": [
+            {
+                "name" : "Jos\u00e9 Mart\u00ednez",
+                "email" : "pepe@organization.com",
+                "userid" : "1"
+            },
+            {
+                "name" : "Dolores Garc\u00eda",
+                "email" : "lola@organization.com",
+                "userid" : "2"
+            }
+        ]
+    }
+];
