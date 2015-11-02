85ae69a95e8f475d7baf48575b90a80b9460f505
Carlos Blanco <cblanco@conwet.com>
2015-11-02 21:11:50 +0100

    Add Product and ProductService real controllers

---
+++ controllers/ProductService.js
@@ -0,0 +1,63 @@
+/*
+
+    #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+      This file is part of the Smart Developer Hub Product:
+        http://www.smartdeveloperhub.org/
+      Center for Open Middleware
+            http://www.centeropenmiddleware.com/
+    #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+      Copyright (C) 2015 Center for Open Middleware.
+    #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+      Licensed under the Apache License, Version 2.0 (the "License");
+      you may not use this file except in compliance with the License.
+      You may obtain a copy of the License at
+                http://www.apache.org/licenses/LICENSE-2.0
+      Unless required by applicable law or agreed to in writing, software
+      distributed under the License is distributed on an "AS IS" BASIS,
+      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+      See the License for the specific language governing permissions and
+     limitations under the License.
+    #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+*/
+
+'use strict';
+
+/**
+ * Get Product List
+ * @param callback Request
+ */
+exports.allProductsInfo = function(callback) {
+    // Get Products from global var
+    callback(products);
+};
+
+/**
+ * Get the information from a specific Product
+ * @param callback To response with the product information
+ * @param prid Product ID
+ */
+exports.productInfo = function(prid, callback) {
+
+    // Check if Product ID is available
+    var _prod;
+    if (prid in productsById) {
+        _prod = underscore(productsById[prid]).clone();
+        // TODO users and repositories
+        var localUsers = usersByProductUri[productUriById[prid]];
+        var productUsers = [];
+        for(var key in localUsers) {
+            productUsers.push(usersById[localUsers[key]]);
+        }
+        _prod['users'] = underscore(productUsers).clone();
+    } else {
+        console.log("--PRID not found");
+        callback(404);
+        return;
+    }
+    sdhWrapper.getProductInfo(prid, function(e) {
+        if (e) {
+            e.users = _prod['users'];
+        }
+        callback(e);
+    });
+};
