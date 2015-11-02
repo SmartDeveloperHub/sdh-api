a6f74001524368cf34c5b512aeea4393dbe0fe25
Carlos Blanco <cblanco@conwet.com>
2015-11-02 21:04:03 +0100

    Add Dummy Organization data

---
+++ sdh/fakeOrganizationsInfo.js
@@ -0,0 +1,46 @@
+/*
+
+    #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+      This file is part of the Smart Developer Hub Project:
+        http://www.smartdeveloperhub.org/
+      Center for Open Middleware
+            http://www.centeropenmiddleware.com/
+    #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+      Copyright (C) 2015 Center for Open Middleware.
+    #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+      Licensed under the Apache License, Version 2.0 (the "License");
+      you may not use this file except in compliance with the License.
+      You may obtain a copy of the License at
+                http://www.apache.org/licenses/LICENSE-2.0
+      Unless required by applicable law or agreed to in writing, software
+      distributed under the License is distributed on an "AS IS" BASIS,
+      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+      See the License for the specific language governing permissions and
+     limitations under the License.
+    #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+*/
+
+'use strict';
+
+exports.fakeOrganizationsInfo = [
+    {
+        "organizationid" : "Organization DePalo 1",
+        "description" : "Center Open Middleware Researches ",
+        "purpose" : "This description explains the purpose of this organization",
+        "clasification": "ALM",
+        "tags" :['SDH','org'],
+        "avatar": "assets/images/CytoChartDemo/rp1.png",
+        "Products": [
+        ]
+    }
+];
+
+exports.organizationsList = [
+    {
+        "organizationid" : "Organization DePalo 1",
+        "description" : "Center Open Middleware Researches ",
+        "purpose" : "This description explains the purpose of this organization",
+        "clasification": "ALM",
+        "avatar": "assets/images/CytoChartDemo/rp1.png"
+    }
+];
