6d8a17ad341ce722b2576ac63d6d402a9c0f1d20
Carlos Blanco <cblanco@conwet.com>
2016-01-14 11:14:30 +0100

    Add new bunyan logs in sessions code

--- sessions.js
+++ sessions.js
@@ -22,86 +22,92 @@
 
 'use strict';
 
-// PRIVATE METHODS
-var hasExpired = function hasExpired(session) {
-    return session['info']['lastAccess'] + SESSION_DURATION < Date.now();
-};
+try {
+    // PRIVATE METHODS
+    var hasExpired = function hasExpired(session) {
+        return session['info']['lastAccess'] + SESSION_DURATION < Date.now();
+    };
 
 
-// PUBLIC METHODS
-var getSession = function getSession(token, checkExpiration) {
+    // PUBLIC METHODS
+    var getSession = function getSession(token, checkExpiration) {
 
-    var session = GLOBAL.SESSIONS[token];
+        var session = GLOBAL.SESSIONS[token];
 
-    if(session == null) {
-        return null;
-    }
+        if(session == null) {
+            return null;
+        }
 
-    //Check expiration. If it has expired delete the session
-    if (checkExpiration && hasExpired(session)) {
-        deleteSession(token);
-        return null;
-    }
+        //Check expiration. If it has expired delete the session
+        if (checkExpiration && hasExpired(session)) {
+            deleteSession(token);
+            return null;
+        }
 
-    return session;
-};
+        return session;
+    };
 
-var deleteSession = function deleteSession(token) {
-    delete GLOBAL.SESSIONS[token];
-};
+    var deleteSession = function deleteSession(token) {
+        delete GLOBAL.SESSIONS[token];
+    };
 
-var touch = function touch(token) {
-    GLOBAL.SESSIONS[token]['info']['lastAccess'] = Date.now();
-};
+    var touch = function touch(token) {
+        GLOBAL.SESSIONS[token]['info']['lastAccess'] = Date.now();
+    };
 
-var getSessionInfo = function getSessionInfo(token, checkExpiration) {
-    var session = getSession(token, checkExpiration);
-    return session == null ? null : session['info'];
-};
+    var getSessionInfo = function getSessionInfo(token, checkExpiration) {
+        var session = getSession(token, checkExpiration);
+        return session == null ? null : session['info'];
+    };
 
-var getSessionData = function getSessionData(token, checkExpiration) {
-    var session = getSession(token, checkExpiration);
-    return session == null ? null : session['data'];
-};
+    var getSessionData = function getSessionData(token, checkExpiration) {
+        var session = getSession(token, checkExpiration);
+        return session == null ? null : session['data'];
+    };
 
-//Get the authentication token of the given request
-var getToken = function(req) {
-    if (req.headers && req.headers.authorization) {
-        var parts = req.headers.authorization.split(' ');
-        if (parts.length == 2) {
-            var scheme = parts[0]
-                , credentials = parts[1];
+    //Get the authentication token of the given request
+    var getToken = function(req) {
+        if (req.headers && req.headers.authorization) {
+            var parts = req.headers.authorization.split(' ');
+            if (parts.length == 2) {
+                var scheme = parts[0]
+                    , credentials = parts[1];
 
-            if (/^Bearer$/i.test(scheme)) {
-                return credentials;
+                if (/^Bearer$/i.test(scheme)) {
+                    return credentials;
+                }
             }
         }
-    }
 
-    return null;
-};
+        return null;
+    };
 
-var initialize = function initialize() {
+    var initialize = function initialize() {
 
-    //Create a lobal variable to store sessions
-    GLOBAL.SESSIONS = {};
+        //Create a lobal variable to store sessions
+        GLOBAL.SESSIONS = {};
 
-    // Session garbage collection...Remove expired sessions
-    setInterval(function() {
+        // Session garbage collection...Remove expired sessions
+        setInterval(function() {
 
-        for(var token in GLOBAL.SESSIONS) {
-            getSession(token, true); //et session can check if it has expired
-        }
+            for(var token in GLOBAL.SESSIONS) {
+                getSession(token, true); //et session can check if it has expired
+            }
 
-    }, SESSION_GARBAGE_COLLECTOR);
+        }, SESSION_GARBAGE_COLLECTOR);
 
-};
+    };
 
-exports.getSession = getSession;
-exports.deleteSession = deleteSession;
-exports.touch = touch;
-exports.getSessionInfo = getSessionInfo;
-exports.getSessionData = getSessionData;
-exports.getToken = getToken;
-exports.initialize = initialize;
+    exports.getSession = getSession;
+    exports.deleteSession = deleteSession;
+    exports.touch = touch;
+    exports.getSessionInfo = getSessionInfo;
+    exports.getSessionData = getSessionData;
+    exports.getToken = getToken;
+    exports.initialize = initialize;
+    log.debug("session utils OK");
+} catch (err){
+    log.error(" ! Error loading authentication util:");
+    log.error(err);
+}
 
