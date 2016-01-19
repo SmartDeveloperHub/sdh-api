11fa9f69e499ca60ca83275b64ba8e14dca4e4c1
Carlos Blanco <cblanco@conwet.com>
2016-01-19 18:21:34 +0100

    Create log path automatically

--- index.js
+++ index.js
@@ -33,11 +33,7 @@
         var bunyan = require('bunyan');
         var PrettyStream = require('bunyan-prettystream');
     } catch (err) {
-        console.error("Fatal API Error. bunyan logs problem: " + err);
-        log.info('Exiting...');
-        setTimeout(function() {
-            process.exit(0);
-        }, 1000);
+        console.error("API Error. bunyan logs problem: " + err);
     }
     try {
         // Set Config params
@@ -49,193 +45,202 @@
             process.exit(0);
         }, 1000);
     }
-    /* Log configuration
-    *   "fatal" (60): The service/app is going to stop or become unusable now. An operator should definitely look into this soon.
-    *   "error" (50): Fatal for a particular request, but the service/app continues servicing other requests. An operator should look at this soon(ish).
-    *   "warn" (40): A note on something that should probably be looked at by an operator eventually.
-    *   "info" (30): Detail on regular operation.
-    *   "debug" (20): Anything else, i.e. too verbose to be included in "info" level.
-    *   "trace" (10): Logging from external libraries used by your app or very detailed application logging.
-    * */
+    /* File Log */
     var prettyStdOut = new PrettyStream();
     prettyStdOut.pipe(process.stdout);
-
-    GLOBAL.log = bunyan.createLogger({
-            name: 'SDH_API',
-            streams: [{
-                level: CONSOLE_LOG_LEVEL,
-                stream: prettyStdOut
-            },
-            {
-                level: FILE_LOG_LEVEL,
-                type: 'rotating-file',
-                path: FILE_LOG_PATH,
-                period: FILE_LOG_PERIOD + 'h',   // daily rotation
-                count: FILE_LOG_NFILES        // keep 3 back copies
-            }]
+    GLOBAL.log = null;
+    GLOBAL.mkdirp = require("mkdirp");
+    GLOBAL.getDirName = require("path").dirname;
+    mkdirp(getDirName(FILE_LOG_PATH), function (err) {
+        if (err) {
+            console.error("! Log file disabled");
+            console.error("Error creating log file " +  FILE_LOG_PATH);
+            log.error(err);
+        } else {
+            log = bunyan.createLogger({
+                    name: 'SDH_API',
+                    streams: [{
+                        level: CONSOLE_LOG_LEVEL,
+                        stream: prettyStdOut
+                    },
+                    {
+                        level: FILE_LOG_LEVEL,
+                        type: 'rotating-file',
+                        path: FILE_LOG_PATH,
+                        period: FILE_LOG_PERIOD + 'h',   // daily rotation
+                        count: FILE_LOG_NFILES        // keep 3 back copies
+                    }]
+            });
+            startAPI();
+        }
     });
 
-    log.info("... Starting SDH-API ...");
+    var startAPI = function startAPI () {
+        log.info("... Starting SDH-API ...");
 
-    // Shut down function
-    var gracefullyShuttinDown = function gracefullyShuttinDown() {
-        log.warn('Shut down signal Received ');
-        log.warn(" ! Shutting Down SDH-API manually.");
-        setTimeout(function() {
-            process.exit(0);
-        }, 1000);
+        // Shut down function
+        var gracefullyShuttinDown = function gracefullyShuttinDown() {
+            log.warn('Shut down signal Received ');
+            log.warn(" ! Shutting Down SDH-API manually.");
+            setTimeout(function () {
+                process.exit(0);
+            }, 1000);
 
-    };
-    // Set security handlers
-    process.on('SIGINT', gracefullyShuttinDown);
-    process.on('SIGTERM', gracefullyShuttinDown);
-
-
-    log.info("... Loading Modules...");
-    try {
-        if (DUMMYDATA) {
-            console.warn("**  !!Attention!! This API is configurated to provide dummy information. If you want to connect to the real SDH-platform, change the DUMMYDATA flag in sdhconfig file");
-        }
-
-        //fs
-        GLOBAL.fs = require('fs');
-        // global sdhTools for log and other basic utils
-        GLOBAL.sdhTools = require('./sdh/sdhTools');
-        // global moment.js
-        GLOBAL.moment = require('moment');
-        // APP
-        var app = require('connect')();
-        GLOBAL.http = require('http');
-        var swaggerTools = require('swagger-tools');
-        GLOBAL.syncRequest = require('sync-request');
-        GLOBAL.request = require('request');
-        // The Swagger document (require it, build it programmatically, fetch it from a URL, ...)
-        var swaggerDoc = require('./api/swagger.json');
-        GLOBAL.underscore = require('underscore');
-        // Rabbitmq
-        GLOBAL.amqp = require('amqplib/callback_api');
-        // n3
-        GLOBAL.N3 = require('n3');
-        // UUID
-        GLOBAL.uuid = require('uuid4');
-        GLOBAL.agentId = uuid(); // uuid 4
-        // Last Update Date
-        GLOBAL.lastUpdate = null;
-    }
-    catch (err) {
-        log.error(" ! Error loading dependencies: " + err);
-        log.info('Exiting...');
-        setTimeout(function() {
-            process.exit(0);
-        }, 1000);
-    }
-    // init server
-    try {
-        // swaggerRouter configuration
-        var options = {
-            swaggerUi: '/swagger.json',
-            controllers: './controllers',
-            useStubs: false // Conditionally turn on stubs (mock mode)
         };
-        var loader = require('./sdh/init');
+        // Set security handlers
+        process.on('SIGINT', gracefullyShuttinDown);
+        process.on('SIGTERM', gracefullyShuttinDown);
 
-        /**
-         * Check for new metrics, tbds, users or repositories
-         * @param frec Number in seconds indicating the refresh rate
-         */
-        var setRefreshRate = function setRefreshRate(frec) {
-            log.info("Setting sdh refresh rate in " + frec + 's');
-            setInterval( function() {
-                log.info("Checking for sdh changes");
-                loadStartDate = moment();
-                loader.update(function() {
-                    var now = moment();
-                    var loadTime = moment.duration(now-loadStartDate).asMilliseconds();
-                    log.info(" API updated!! " + now.format() + "--- ( " + loadTime/1000 + " seconds )");
-                });
-            },frec*1000);
-        };
 
-        // Initializice session support
-        require("./sessions").initialize();
-
-        // Initialize authentication (needs session support)
-        require("./authentication").initialize();
-
-        app.use(function(req, res, next){
-            res.setHeader('Access-Control-Allow-Origin', '*');
-            res.setHeader('Access-Control-Allow-Credentials', 'true');
-            if (req.method == 'OPTIONS')
-            {
-                res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-                res.setHeader('Access-Control-Max-Age', '604800');
-                //if you need special headers
-                res.setHeader('Access-Control-Allow-Headers', 'x-requested-with, authorization');
-                res.end();
-            } else {
-                next();
+        log.info("... Loading Modules...");
+        try {
+            if (DUMMYDATA) {
+                console.warn("**  !!Attention!! This API is configurated to provide dummy information. If you want to connect to the real SDH-platform, change the DUMMYDATA flag in sdhconfig file");
             }
-        });
 
-        /**
-         * Launch API with Swagger. Auto-validation, link endpoints with controllers using
-         * swagger.json and generate an interactive documentation with Swagger-UI
-         */
-        var launchSwaggerAPI = function() {
-            // Initialize the Swagger middleware
-            swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
-                // Interpret Swagger resources and attach metadata to request - must be first in swagger-tools middleware chain
-                app.use(middleware.swaggerMetadata());
+            //fs
+            GLOBAL.fs = require('fs');
+            // global sdhTools for log and other basic utils
+            GLOBAL.sdhTools = require('./sdh/sdhTools');
+            // global moment.js
+            GLOBAL.moment = require('moment');
+            // APP
+            var app = require('connect')();
+            GLOBAL.http = require('http');
+            var swaggerTools = require('swagger-tools');
+            GLOBAL.syncRequest = require('sync-request');
+            GLOBAL.request = require('request');
+            // The Swagger document (require it, build it programmatically, fetch it from a URL, ...)
+            var swaggerDoc = require('./api/swagger.json');
+            GLOBAL.underscore = require('underscore');
+            // Rabbitmq
+            GLOBAL.amqp = require('amqplib/callback_api');
+            // n3
+            GLOBAL.N3 = require('n3');
+            // UUID
+            GLOBAL.uuid = require('uuid4');
+            GLOBAL.agentId = uuid(); // uuid 4
+            // Last Update Date
+            GLOBAL.lastUpdate = null;
+        }
+        catch (err) {
+            log.error(" ! Error loading dependencies: " + err);
+            log.info('Exiting...');
+            setTimeout(function () {
+                process.exit(0);
+            }, 1000);
+        }
+        // init server
+        try {
+            // swaggerRouter configuration
+            var options = {
+                swaggerUi: '/swagger.json',
+                controllers: './controllers',
+                useStubs: false // Conditionally turn on stubs (mock mode)
+            };
+            var loader = require('./sdh/init');
 
-                // Validate Swagger requests
-                app.use(middleware.swaggerValidator());
+            /**
+             * Check for new metrics, tbds, users or repositories
+             * @param frec Number in seconds indicating the refresh rate
+             */
+            var setRefreshRate = function setRefreshRate(frec) {
+                log.info("Setting sdh refresh rate in " + frec + 's');
+                setInterval(function () {
+                    log.info("Checking for sdh changes");
+                    loadStartDate = moment();
+                    loader.update(function (isBackup) {
+                        if (isBackup) {
+                            return;
+                        } else {
+                            var now = moment();
+                            var loadTime = moment.duration(now - loadStartDate).asMilliseconds();
+                            log.info(" API updated!! " + now.format() + "--- ( " + loadTime / 1000 + " seconds )");
+                        }
+                    });
+                }, frec * 1000);
+            };
 
-                // Route validated requests to appropriate controller
-                app.use(middleware.swaggerRouter(options));
+            // Initializice session support
+            require("./sessions").initialize();
 
-                // Serve the Swagger documents and Swagger UI
-                app.use(middleware.swaggerUi());
-                // Take local IP in URL is null
-                if (SWAGGER_URL == null) {
-                    SWAGGER_URL = "http://localhost";
+            // Initialize authentication (needs session support)
+            require("./authentication").initialize();
+
+            app.use(function (req, res, next) {
+                res.setHeader('Access-Control-Allow-Origin', '*');
+                res.setHeader('Access-Control-Allow-Credentials', 'true');
+                if (req.method == 'OPTIONS') {
+                    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+                    res.setHeader('Access-Control-Max-Age', '604800');
+                    //if you need special headers
+                    res.setHeader('Access-Control-Allow-Headers', 'x-requested-with, authorization');
+                    res.end();
+                } else {
+                    next();
                 }
-                // Start the server
-                http.createServer(app).listen(SWAGGER_PORT, function () {
-                    setRefreshRate(REFRESH_RATE);
-                    var now = moment();
-                    loadStartDate = moment(loadStartDate);
-                    var loadTime = moment.duration(now-loadStartDate).asMilliseconds();
-                    log.info("---    SDH-API Ready!!   --- ( " + loadTime/1000 + " seconds )");
-                    log.info('SDH-API is listening (' + SWAGGER_URL + ':' + SWAGGER_PORT + ')');
-                    log.info('SDH-API Swagger-ui is available on ' + SWAGGER_URL + ':' + SWAGGER_PORT + '/docs');
-                });
-
-                // Fork workers. No comparten nada. Opcion 1 para clusterizar. Base de datos con las variables globales
-                // y que master sea el que actualice estas cosas mientras el resto de procesos escuchan a las
-                // peticiones de la api.
-                // opcion 2 que cada proceso pida su propia mierda al agora y vayan por libre
-                // opcion 3 que los hilos se creen cuando se tengan las variables locales y master se las pase uno a uno
-                // para simular que se comparten
-                /*for (var i = 0; i < numCPUs; i++) {
-                    cluster.fork();
-                }*/
             });
-        };
-        process.on('uncaughtException', function (err) {
-            log.error(err);
-            log.warn("Something is wrong, but SDH will continue on line");
-        });
-        loader.init(launchSwaggerAPI);
-    }
-    catch (err) {
-        log.error("Error loading initial data from SDH-Platform: ");
-        log.error(err);
-        log.info('Exiting...');
-        setTimeout(function() {
-            process.exit(0);
-        }, 1000);
 
-    }
+            /**
+             * Launch API with Swagger. Auto-validation, link endpoints with controllers using
+             * swagger.json and generate an interactive documentation with Swagger-UI
+             */
+            var launchSwaggerAPI = function () {
+                // Initialize the Swagger middleware
+                swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
+                    // Interpret Swagger resources and attach metadata to request - must be first in swagger-tools middleware chain
+                    app.use(middleware.swaggerMetadata());
+
+                    // Validate Swagger requests
+                    app.use(middleware.swaggerValidator());
+
+                    // Route validated requests to appropriate controller
+                    app.use(middleware.swaggerRouter(options));
+
+                    // Serve the Swagger documents and Swagger UI
+                    app.use(middleware.swaggerUi());
+                    // Take local IP in URL is null
+                    if (SWAGGER_URL == null) {
+                        SWAGGER_URL = "http://localhost";
+                    }
+                    // Start the server
+                    http.createServer(app).listen(SWAGGER_PORT, function () {
+                        setRefreshRate(REFRESH_RATE);
+                        var now = moment();
+                        loadStartDate = moment(loadStartDate);
+                        var loadTime = moment.duration(now - loadStartDate).asMilliseconds();
+                        log.info("---    SDH-API Ready!!   --- ( " + loadTime / 1000 + " seconds )");
+                        log.info('SDH-API is listening (' + SWAGGER_URL + ':' + SWAGGER_PORT + ')');
+                        log.info('SDH-API Swagger-ui is available on ' + SWAGGER_URL + ':' + SWAGGER_PORT + '/docs');
+                    });
+
+                    // Fork workers. No comparten nada. Opcion 1 para clusterizar. Base de datos con las variables globales
+                    // y que master sea el que actualice estas cosas mientras el resto de procesos escuchan a las
+                    // peticiones de la api.
+                    // opcion 2 que cada proceso pida su propia mierda al agora y vayan por libre
+                    // opcion 3 que los hilos se creen cuando se tengan las variables locales y master se las pase uno a uno
+                    // para simular que se comparten
+                    /*for (var i = 0; i < numCPUs; i++) {
+                     cluster.fork();
+                     }*/
+                });
+            };
+            process.on('uncaughtException', function (err) {
+                log.error(err);
+                log.warn("Something is wrong, but SDH will continue on line");
+            });
+            loader.init(launchSwaggerAPI);
+        }
+        catch (err) {
+            log.error("Error loading initial data from SDH-Platform: ");
+            log.error(err);
+            log.info('Exiting...');
+            setTimeout(function () {
+                process.exit(0);
+            }, 1000);
+
+        }
+    };
 //} else {
     // forks
     // Start the server
